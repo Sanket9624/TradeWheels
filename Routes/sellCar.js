a8b5e9231441326bbec7a50c7cfbf5968f0ad322
sp960 <sanket.prajapati0906@gmail.com>
2024-08-08 11:59:42 +0530

    authetication not needed to access car

--- Routes/sellCar.js
+++ Routes/sellCar.js
@@ -5,9 +5,9 @@ const car = require('../models/car');
 const router = express.Router();
 
 router.post('/',authMiddleware.authenticateToken,sellCarController.sellCar);
-router.get('/cars', authMiddleware.authenticateToken, sellCarController.getAllCars);
-router.get('/cars/:id', authMiddleware.authenticateToken, sellCarController.getCarById); 
-router.get('/user/cars', authMiddleware.authenticateToken, sellCarController.getCarsByUserId);
+router.get('/cars', sellCarController.getAllCars);
+router.get('/cars/:id',sellCarController.getCarById); 
+router.get('/user/cars',authMiddleware.authenticateToken, sellCarController.getCarsByUserId);
 router.put('/cars/:id', authMiddleware.authenticateToken, sellCarController.updateCar); 
 router.delete('/cars/:id', authMiddleware.authenticateToken, sellCarController.deleteCarById);
 
