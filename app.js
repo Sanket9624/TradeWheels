2169551f73f8a2a99cee4bd7a57278224dd66051
sp960 <sanket.prajapati0906@gmail.com>
2024-08-14 19:50:42 +0530

    cors add

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors);
+
+app.use(cors({
+    origin: '*',
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
